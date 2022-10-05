5a0487474b663f6fb689e40bd5aca2d7c9bf39e4
Dustin Brooks <dbrooks@linuxmail.org>
2022-10-05 13:56:30 -0500

    Add files via upload

--- surf/surf.c
+++ surf/surf.c
@@ -236,7 +236,6 @@ static void togglefullscreen(Client *c, const Arg *a);
 static void togglecookiepolicy(Client *c, const Arg *a);
 static void toggleinspector(Client *c, const Arg *a);
 static void find(Client *c, const Arg *a);
-static void search(Client *c, const Arg *a);
 
 /* Buttons */
 static void clicknavigate(Client *c, const Arg *a, WebKitHitTestResult *h);
@@ -342,7 +341,6 @@ setup(void)
 
 	/* atoms */
 	atoms[AtomFind] = XInternAtom(dpy, "_SURF_FIND", False);
-	atoms[AtomSearch] = XInternAtom(dpy, "_SURF_SEARCH", False);
 	atoms[AtomGo] = XInternAtom(dpy, "_SURF_GO", False);
 	atoms[AtomUri] = XInternAtom(dpy, "_SURF_URI", False);
 	atoms[AtomUTF8] = XInternAtom(dpy, "UTF8_STRING", False);
@@ -597,19 +595,6 @@ loaduri(Client *c, const Arg *a)
 		webkit_web_view_load_uri(c->view, url);
 		updatetitle(c);
 	}
-
-	g_free(url);
-}
-
-void
-search(Client *c, const Arg *a)
-{
-	Arg arg;
-	char *url;
-
-	url = g_strdup_printf(searchurl, a->v);
-	arg.v = url;
-	loaduri(c, &arg);
 
 	g_free(url);
 }
@@ -1351,9 +1336,6 @@ processx(GdkXEvent *e, GdkEvent *event, gpointer d)
 				find(c, NULL);
 
 				return GDK_FILTER_REMOVE;
-			} else if (ev->atom == atoms[AtomSearch]) {
-				a.v = getatom(c, AtomSearch);
-				search(c, &a);
 			} else if (ev->atom == atoms[AtomGo]) {
 				a.v = getatom(c, AtomGo);
 				loaduri(c, &a);
---
+++ web-browser/start-dweb.sh
@@ -0,0 +1,1 @@
+python3 /home/dustin/web-browser/tabbedbrowser.py
